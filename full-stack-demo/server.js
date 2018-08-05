435ccaead07ed24d444edb44c190dcd976a5a981
gta191977649 <episodes27@gmail.com>
2018-08-05 17:21:09 +1000

    add simple server hello world demo

--- full-stack-demo/server.js
+++ full-stack-demo/server.js
@@ -1,1 +1,13 @@
-import config from './config';
+import config from './config'
+import express from 'express'
+const server = express()
+
+server.get('/',(req,res) => {
+    res.send("Hello world index")
+});
+server.get('/500',(req,res) => {
+    res.send("Hello world 500~")
+});
+server.listen(config.port,()=> {
+    console.info('Server is run at port ->',config.port)
+});
